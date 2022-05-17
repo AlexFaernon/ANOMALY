373be57a6438631c00138174236f138948ca459e
sckmat <schmat03@gmail.com>
2022-05-17 21:02:56 +0500

    Added Map Flip

---
+++ Assets/scripts/GlobalMap/Flip.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Flip : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> sprites = new List<Sprite>();
+    [SerializeField] private Image panel;
+    [SerializeField] private Button Forward;
+    [SerializeField] private Button Back;
+    private int index = 0;
+
+    private void Awake()
+    {
+        Forward.onClick.AddListener(() =>OnClickForward());
+        Back.onClick.AddListener(() => OnClickBack());
+    }
+    private void OnClickForward()
+    {
+        if (sprites.Count == index + 1)
+            return;
+
+        index++;
+        panel.sprite = sprites[index];
+    }
+
+    private void OnClickBack()
+    {
+        if (index == 0)
+            return;
+
+        index --;
+        panel.sprite = sprites[index];
+    }
+}
