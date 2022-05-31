6f13e9c64c18721618ab5d86c9e4c7681751f753
AlexFaernon <alexpavlenko6666@rambler.ru>
2022-05-31 18:46:27 +0500

    Uh...

--- Assets/scripts/Map/NodeScript.cs
+++ Assets/scripts/Map/NodeScript.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using Random = System.Random;
 
 public class NodeScript : MonoBehaviour
 {
